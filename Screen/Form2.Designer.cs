979a539cd8a0eb287cccb2a192745937fa0ff394
P4ivvaz <livia.psousa@senacsp.edu.br>
2025-03-18 19:20:32 -0300

    Duas telasAdicionar arquivos de projeto.

---
+++ Screen/Form2.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace LayoutTelaPI
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Vivaldi", 27.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(263, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(233, 44);
+            label1.TabIndex = 0;
+            label1.Text = "Beyoncé Diva";
+            label1.TextAlign = ContentAlignment.BottomCenter;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(375, 62);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(413, 388);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(67, 62);
+            button1.Name = "button1";
+            button1.Size = new Size(121, 65);
+            button1.TabIndex = 2;
+            button1.Text = "Abrir Nova Janela";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(67, 143);
+            button2.Name = "button2";
+            button2.Size = new Size(121, 67);
+            button2.TabIndex = 3;
+            button2.Text = "Abrir Janela";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.MediumPurple;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Name = "Form2";
+            Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button button2;
+    }
+}
